1a9ff9b88d3845067782799bfd1d85ea589c88cd
Chuck Bardel <bardelch@msu.edu>
2018-07-10 15:42:51 -0400

    use env file for run and link containers

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -e passwords.txt ]
+then
+	./setup.sh
+fi
+
+docker stop postgres
+docker stop arachni
+./cleanImages.sh
+
+#source passwords.txt
+
+#docker run --name postgres -e POSTGRES_PASSWORD=$POSTGRES_PASS -e POSTGRES_USER=arachni -e POSTGRES_DB=arachni_production -d postgres
+#docker run -t --name arachni -e POSTGRES_PASSWORD=$POSTGRES_PASS --link postgres:db -p 9292:9292 bardelch/arachni
+#docker exec -i arachni /opt/arachni/bin/arachni_web_task db:setup
+
+
+echo docker run --name postgres --env-file password.txt -d postgres
+docker run --name postgres --env-file password.txt -d postgres
+
+echo docker run -t --name arachni --env-file password.txt --link postgres:db -p 9292:9292 bardelch/arachni
+docker run -t --name arachni --env-file password.txt --link postgres:db -p 9292:9292 bardelch/arachni
+
+echo docker exec -i arachni /opt/arachni/bin/arachni_web_task db:setup
+docker exec -i arachni /opt/arachni/bin/arachni_web_task db:setup
+
