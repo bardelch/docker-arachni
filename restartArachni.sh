624944eb44e6c06c1a3478212f0075540227f853
Charles Bardel <bardelch@msu.edu>
2018-09-18 15:27:30 -0400

    add restart arachni script

--- restartArachni.sh
+++ restartArachni.sh
@@ -2,6 +2,8 @@
 
 docker stop arachni
 
+./cleanImages.sh
+
 echo docker run -t --name arachni --env-file password.txt --link postgres:db -p 9292:9292 bardelch/arachni
 docker run -t --name arachni --env-file password.txt --link postgres:db -p 9292:9292 bardelch/arachni
 
